c36a0e435e54902d36e366daf0b6a6d7a2e29da3
Durgesh <durgesh@example.com>
2025-12-31 12:17:39 +0530

    Test auto deploy

--- app.js
+++ app.js
@@ -2,5 +2,7 @@ const http = require('http');
 
 http.createServer((req, res) => {
   res.end("CI/CD Auto deploy Done Successfully 🚀");
-}).listen(3000);
+}).listen(3000, '0.0.0.0', () => {
+  console.log("Server running on port 3000");
+});
 
